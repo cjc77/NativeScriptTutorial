834ed5713f85f321f1f420915f4bcf2ece21cb47
carson <carsonjamescook@gmail.com>
2016-11-27 22:51:49 -0800

    Created Login process and hardcoded a username/password for further testing.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,8 +1,11 @@
 import { Component } from "@angular/core";
 import { User } from "./shared/user/user";
+import { UserService } from "./shared/user/user.service";
 
 @Component({
   selector: "my-app",
+  // providers is a list of all angular services
+  providers: [UserService],
   templateUrl: "pages/login/login.html",
   styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
@@ -11,11 +14,30 @@ export class AppComponent {
     user: User;
     isLoggingIn = true;
 
-    constructor() {
+    constructor(private userService: UserService) {
         this.user = new User();
+        this.user.email = "baja@gmail.com";
+        this.user.password = "password";
     }
     submit() {
-        alert("You're using: " + this.user.email)
+        if(this.isLoggingIn) {
+            this.login();
+        } else {
+            this.signUp();
+        }
+    }
+    login() {
+        // TODO: Define
+    }
+    signUp() {
+        this.userService.register(this.user)
+            .subscribe(
+                () => {
+                    alert("Your account was successfully created.");
+                    this.toggleDisplay();
+                },
+                () => alert("We were unable to create your account.")
+            );
     }
     toggleDisplay() {
         this.isLoggingIn = !this.isLoggingIn;
